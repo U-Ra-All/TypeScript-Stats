86a1a1aaba4b61cb4586b5977602f751517e3fd4
YouRa <allakhverdovyuriy@gmail.com>
2021-05-27 10:10:41 +0300

    Read CSV

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hey there!');
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+})
+.split('\n')
+.map((row: string): string[] => {
+  return row.split(',');
+});
+
+console.log(matches);
