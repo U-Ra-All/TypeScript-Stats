9eb5beca84ee5e84b59991527f588228769c3fbb
YouRa <allakhverdovyuriy@gmail.com>
2021-05-28 16:45:30 +0300

    Альтернативный рефакторинг' git commit -m 'Альтернативный рефакторинг

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,13 @@
 import fs from 'fs';
+import { dateStringToDate } from './util';
+import { MatchResult } from './MatchResult';
 
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class CsvFileReader {
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
-
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -15,6 +17,16 @@ export abstract class CsvFileReader<T> {
     .map((row: string): string[] => {
       return row.split(',');
     })
-    .map(this.mapRow);
+    .map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult, // 'H', 'A', 'D'
+        row[6]
+      ];
+    });
   }
 }
