1e37f8784f49a39a0efec06a9eb1484b9756f175
YouRa <allakhverdovyuriy@gmail.com>
2021-05-31 11:20:05 +0300

    Трансформация данных

--- build/MatchReader.js
+++ build/MatchReader.js
@@ -1,10 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MatchReader = void 0;
+var util_1 = require("./util");
 var MatchReader = /** @class */ (function () {
     function MatchReader(reader) {
         this.reader = reader;
+        this.matches = [];
     }
+    MatchReader.prototype.load = function () {
+        this.reader.read();
+        this.matches = this.reader.data.map(function (row) {
+            return [
+                util_1.dateStringToDate(row[0]),
+                row[1],
+                row[2],
+                parseInt(row[3]),
+                parseInt(row[4]),
+                row[5],
+                row[6]
+            ];
+        });
+    };
     return MatchReader;
 }());
 exports.MatchReader = MatchReader;
