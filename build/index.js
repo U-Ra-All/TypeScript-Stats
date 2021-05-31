ececffef4648e1196752655ee7ac913a4af5dde7
YouRa <allakhverdovyuriy@gmail.com>
2021-05-31 11:57:37 +0300

    Обновление ссылок Reader

--- build/index.js
+++ build/index.js
@@ -2,11 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var MatchResult_1 = require("./MatchResult");
-var reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
-// console.log(reader.data);
+var CsvFileReader_1 = require("./CsvFileReader");
+// Create an object that implements the DataReader interface
+var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// Create an object of MatchReader and pass DataReader implementation
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
 var manUnitedWins = 0;
-for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
     var match = _a[_i];
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// Create an object that implements the DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-// console.log(reader.data);
+// Create an object of MatchReader and pass DataReader implementation
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+// matchReader.matches
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
