bc395e58ba4d7bea22efafa35f4f31d0cc045d31
YouRa <allakhverdovyuriy@gmail.com>
2021-06-01 08:14:26 +0300

    Очень частое недопонимание концепции композиции

--- build/example.js
+++ build/example.js
@@ -22,3 +22,28 @@
 // holdNumber.data = 111;
 // const holdString = new HoldAnything<string>();
 // holdString.data = 'sadf';
+// Compostion misconception
+// const rectangular = (state) => {
+//   return {
+//     area: () => {
+//       return state.height * state.width;
+//     }
+//   };
+// };
+// const openable = (state) => {
+//   return {
+//     toggleOpen: () => {
+//       state.open = !state.open;
+//     },
+//     area: () => {
+//     }
+//   };
+// };
+// const buildRectangleWindow = (state) => {
+//   return Object.assign(state, rectangular(state), openable(state));
+// };
+// const rectangleWindow = buildRectangleWindow({
+//   height: 30,
+//   width: 20,
+//   open: false
+// });
