206a7b0ce056646a168611c902c109be1639de26
YouRa <allakhverdovyuriy@gmail.com>
2021-06-01 11:31:27 +0300

    Создание Reporter

--- build/Summary.js
+++ build/Summary.js
@@ -2,10 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Summary = void 0;
 var Summary = /** @class */ (function () {
-    function Summary(analyzer, outputTarger) {
+    function Summary(analyzer, outputTarget) {
         this.analyzer = analyzer;
-        this.outputTarger = outputTarger;
+        this.outputTarget = outputTarget;
     }
+    Summary.prototype.buildAndPrintReport = function (matches) {
+        var output = this.analyzer.run(matches);
+        this.outputTarget.print(output);
+    };
     return Summary;
 }());
 exports.Summary = Summary;
