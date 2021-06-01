4ada79ec994d90f5e130707b9852b6446b6de227
YouRa <allakhverdovyuriy@gmail.com>
2021-06-01 10:48:33 +0300

    Имплементация класса Analyzer

--- build/index.js
+++ build/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var MatchResult_1 = require("./MatchResult");
 var CsvFileReader_1 = require("./CsvFileReader");
 // Create an object that implements the DataReader interface
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
@@ -9,14 +8,4 @@ var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 // matchReader.matches
-var manUnitedWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
 console.log("Man United won " + manUnitedWins + " times.");
--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,6 @@ matchReader.load();
 
 // matchReader.matches
 
-let manUnitedWins = 0;
 
-for (let match of matchReader.matches) {
-  if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
 
 console.log(`Man United won ${manUnitedWins} times.`);
