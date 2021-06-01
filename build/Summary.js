6cb1bddc8694073239b617e75f56903602a81f07
YouRa <allakhverdovyuriy@gmail.com>
2021-06-01 10:17:41 +0300

    Подход на основе композиции

---
+++ build/Summary.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+var Summary = /** @class */ (function () {
+    function Summary(analyzer, outputTarger) {
+        this.analyzer = analyzer;
+        this.outputTarger = outputTarger;
+    }
+    return Summary;
+}());
+exports.Summary = Summary;
